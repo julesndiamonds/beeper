ccb3b70fd2cb0d24a632b60ead2390fa31bcd10c
julesndiamonds <julietkellogg@gmail.com>
2016-08-17 23:52:45 -0300

    Update beeper.js

--- beeper.js
+++ beeper.js
@@ -1,6 +1,12 @@
-<script>
-function beeper(){
-	document.('h1')('#button') .addEventListener('click' function()){
-		$ ('footer') .append('div class= "blackborder"> "Beep"> </div>')
-	}
-</script>
+<script type ="text/javascript"></script>
+
+	<script>
+	var button = document.createElement('button');
+	button.innerText = 'click me';
+	document.body.appendChild(button);
+	button.addEventListener('click', function(){
+		$ ('p').append('beep');
+	var randomColor = '#' + Math.random().toString(16).slice(2, 8);
+	$ ('p') .css('background-color', randomColor);
+	});
+	</script>
